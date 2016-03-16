74f587ecc9d7949a5f46c3795f24b87cb53ad746
Andrii PETROVYCH <a.petrovych@levi9.com>
2016-03-16 16:17:37 +0200

    cleanup added new tests

--- src/main/java/com/levi/restboot/controller/UserController.java
+++ src/main/java/com/levi/restboot/controller/UserController.java
@@ -3,48 +3,49 @@ package com.levi.restboot.controller;
 import com.levi.restboot.entity.User;
 import com.levi.restboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping(value = "/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    public User createUser(@RequestBody User user) {
-        return userService.createUser(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+
+        User createdUser = userService.createUser(user);
+
+        return new ResponseEntity<User>(createdUser, HttpStatus.CREATED);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
-    @ResponseBody
-    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         if (userService.getUserById(id) == null) {
-            // TODO return proper answer
-            return null;
+            return new ResponseEntity<User>(HttpStatus.NOT_MODIFIED);
         }
         user.setId(id);
-        return userService.updateUser(user);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
     }
 
     @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-    @ResponseBody
     public void deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
     }
--- src/test/java/com/levi/restboot/controller/UserControllerTest.java
+++ src/test/java/com/levi/restboot/controller/UserControllerTest.java
@@ -10,15 +10,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,11 +35,15 @@ public class UserControllerTest {
     private static UserController userController;
     List<User> users = new ArrayList<>();
     private MockMvc mockMvc;
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
+    private MediaType contentType = MediaType.APPLICATION_JSON_UTF8;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+        this.mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
 
         users.add(new User(1L, "Andrew", "Petrovych", "+380632528"));
         users.add(new User(2L, "Sasha", "Petrovych", "+380632528"));
@@ -77,9 +86,72 @@ public class UserControllerTest {
         Assert.assertEquals(id, userFromResponse.getId());
 
         verify(userService, times(1)).getUserById(idOfNotExistingUser);
-        resultPositive.andExpect(status().isOk());
-        resultPositive.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        resultNegative.andExpect(status().isOk());
         int contentLength = resultNegative.andReturn().getResponse().getContentLength();
         Assert.assertEquals(0, contentLength);
+    }
+
+
+    @Test
+    public void testAddingUser() throws Exception {
+        //given
+        User userToCreate = new User(5L, "Valentyn", "Strykalo", "+3685426");
+        when(userService.createUser(userToCreate)).thenReturn(userToCreate);
+        String userJson = json(userToCreate);
+
+        //when
+        ResultActions result = mockMvc.perform(post("/users").content(userJson).contentType(contentType));
+
+        //then
+        verify(userService, times(1)).createUser(any(User.class));
+        result.andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testDeleteUser() throws Exception {
+        //given
+        long id = 1;
+        doNothing().when(userService).deleteUser(id);
+
+        //when
+        ResultActions result = mockMvc.perform(delete("/users/{id}", id).contentType(contentType));
+
+        //then
+        verify(userService, times(1)).deleteUser(id);
+        result.andExpect(status().isOk());
+        result.andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        //given
+        long idOfExistingUser = 1;
+        User existingUser = users.stream().filter(user -> user.getId() == idOfExistingUser).findFirst().get();
+
+        long idOfNotExistingUser = 5;
+        User notExistingUser = new User(idOfNotExistingUser, "Not", "Exists", "+3555");
+
+        when(userService.getUserById(idOfExistingUser)).thenReturn(existingUser);
+        when(userService.getUserById(idOfNotExistingUser)).thenReturn(null);
+
+        when(userService.updateUser(existingUser)).thenReturn(existingUser);
+
+        //when
+        ResultActions resultPositive = mockMvc.perform(put("/users/{id}", idOfExistingUser).content(json(existingUser)).contentType(contentType));
+        ResultActions resultNegative = mockMvc.perform(put("/users/{id}", idOfNotExistingUser).content(json(notExistingUser)).contentType(contentType));
+
+        //then
+        verify(userService, times(1)).updateUser(any(User.class));
+        resultPositive.andExpect(status().isOk());
+
+        verify(userService, times(1)).getUserById(idOfNotExistingUser);
+        resultNegative.andExpect(status().isNotModified());
+    }
+
+    private String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 }
