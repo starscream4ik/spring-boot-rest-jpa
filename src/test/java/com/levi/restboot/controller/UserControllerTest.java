009c90c9544921cb4d4b4e3cb70f6cddc89f57a8
Andrii PETROVYCH <a.petrovych@levi9.com>
2016-03-16 13:04:50 +0200

    added test for UserController

--- src/test/java/com/levi/restboot/controller/UserControllerTest.java
+++ src/test/java/com/levi/restboot/controller/UserControllerTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -28,30 +28,60 @@ public class UserControllerTest {
     private static UserService userService;
     @InjectMocks
     private static UserController userController;
+    List<User> users = new ArrayList<>();
     private MockMvc mockMvc;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
+        users.add(new User(1L, "Andrew", "Petrovych", "+380632528"));
+        users.add(new User(2L, "Sasha", "Petrovych", "+380632528"));
+        users.add(new User(3L, "Petya", "Petrovych", "+380632528"));
     }
 
     @Test
     public void testGetAllUsers() throws Exception {
         //given
-        List<User> users = new ArrayList<>();
-        users.add(new User(1L, "Andrew", "Petrovych", "+380632528"));
-        users.add(new User(2L, "Sasha", "Petrovych", "+380632528"));
-        users.add(new User(3L, "Petya", "Petrovych", "+380632528"));
         when(userService.getAllUsers()).thenReturn(users);
 
         //when
         ResultActions result = mockMvc.perform(get("/users"));
 
         //then
+        verify(userService, times(1)).getAllUsers();
         result.andExpect(status().isOk());
         result.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
         List usersFromResponse = new ObjectMapper().readValue(result.andReturn().getResponse().getContentAsByteArray(), List.class);
         Assert.assertEquals(usersFromResponse.size(), users.size());
+    }
+
+    @Test
+    public void testGettingUserById() throws Exception {
+        //given
+        long id = 1;
+        long idOfNotExistingUser = 5;
+        when(userService.getUserById(id)).thenReturn(users.stream().filter(user -> user.getId() == id).findFirst().get());
+        when(userService.getUserById(idOfNotExistingUser)).thenReturn(null);
+
+        //when
+        ResultActions resultPositive = mockMvc.perform(get("/users/{id}", id));
+        ResultActions resultNegative = mockMvc.perform(get("/users/{id}", idOfNotExistingUser));
+
+        //then
+        verify(userService, times(1)).getUserById(id);
+        resultPositive.andExpect(status().isOk());
+        resultPositive.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        User userFromResponse = new ObjectMapper().readValue(resultPositive.andReturn().getResponse().getContentAsByteArray(), User.class);
+        Assert.assertEquals(id, userFromResponse.getId());
+
+        verify(userService, times(1)).getUserById(idOfNotExistingUser);
+        resultPositive.andExpect(status().isOk());
+        resultPositive.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+        int contentLength = resultNegative.andReturn().getResponse().getContentLength();
+        Assert.assertEquals(0, contentLength);
+
+
     }
 }
